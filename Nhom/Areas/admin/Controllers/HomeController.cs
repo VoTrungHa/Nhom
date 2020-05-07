51e37e5099e36a161b14847cce55cb551c04b8ca
Vo Trung Ha <trinhsatvth@gmail.com>
2020-05-07 09:47:13 +0700

    area admin

---
+++ Nhom/Areas/admin/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Nhom.Areas.admin.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /admin/Home/
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [ChildActionOnly]
+        public ActionResult BannerMain()
+        {
+            return PartialView();
+        }
+	}
+}
---
+++ Nhom/Areas/admin/adminAreaRegistration.cs
@@ -0,0 +1,25 @@
+﻿using System.Web.Mvc;
+
+namespace Nhom.Areas.admin
+{
+    public class adminAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "admin";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+     "Admin_default",
+     "Admin/{controller}/{action}/{id}",
+     new { action = "Index", id = UrlParameter.Optional },
+     new[] { "Nhom.Areas.admin.Controllers" }
+ );
+        }
+    }
+}
